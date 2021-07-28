4c756ef2843082286253dfc1039a2a2b3a50806f
Khairil Rizal <manjacat@gmail.com>
2021-07-28 15:26:13 +0800

    Added .Net Core

---
+++ Solutions/NetCoreMVC/NetCoreMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            List<Employee> emp = Employee.GetSampleEmployee();
+            return View(emp);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                //Add insert script here
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                //Add Edit script here
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Solutions/NetCoreMVC/NetCoreMVC/Models/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVC.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+
+        }
+
+        public int EmployeeID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        #region methods
+
+        public static List<Employee> GetSampleEmployee()
+        {
+            Employee e1 = new Employee { EmployeeID = 1, Name = "Samad", Department = "IT" };
+            Employee e2 = new Employee { EmployeeID = 2, Name = "Ah Kaw", Department = "Sales" };
+            Employee e3 = new Employee { EmployeeID = 3, Name = "Muthusamy", Department = "Account" };
+
+            List<Employee> emp = new List<Employee>();
+            emp.Add(e1);
+            emp.Add(e2);
+            emp.Add(e3);
+
+            return emp;
+        }
+
+        #endregion
+
+    }
+}
